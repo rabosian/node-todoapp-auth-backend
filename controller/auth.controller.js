3fe2d0730067ae30eb959fd3c3468be163e7de36
rabosian <msann1356@gmail.com>
2024-10-19 16:09:59 -0400

    authController added

---
+++ controller/auth.controller.js
@@ -0,0 +1,26 @@
+const authController = {};
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+require("dotenv").config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) {
+      throw new Error("Invalid token");
+    }
+    token = tokenString.replace("Bearer ", "");
+    jwt.verify(token, JWT_SECRET, (err, payload) => {
+      if (err) {
+        throw new Error("Invalid token");
+      }
+      req.userId = payload._id;
+    });
+    next();
+  } catch (err) {
+    res.status(400).json({ status: "Failed", error: err.message });
+  }
+};
+
+module.exports = authController;
